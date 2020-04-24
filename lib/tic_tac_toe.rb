821622e8c23360d2b290f170c3659ef83b1228bd
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-24 22:02:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ end
 
 def turn
    puts "Please enter 1-9:"
-user_input = gets.strip
+user_input = gets.chomp
 index= input_to_index(user_input)
  if valid_move?(index)
  move(index, current_player)
