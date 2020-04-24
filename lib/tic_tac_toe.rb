ff1e27851c17c137043df600045e8cb2518b74b5
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-24 22:06:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,6 @@
  class TicTacToe
+   
+   
   def initialize
     @board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
@@ -68,16 +70,7 @@ end
 return current_player
 end
 
- WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
-]
+ 
    
 def won?
  
